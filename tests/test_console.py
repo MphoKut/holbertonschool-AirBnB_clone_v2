e58ff4efbaa7a687b0fd01735f1dd3ec3524c9eb
Mpho Kutoane <mkutoane@singular.co.za>
2022-08-10 13:43:56 +0200

    AirBnB SQL

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,11 +3,14 @@
 Contains the class TestConsoleDocs
 """
 
-import console
-import inspect
 import pep8
 import unittest
-HBNBCommand = console.HBNBCommand
+import models
+from unittest.mock import patch
+from io import StringIO
+from console import HBNBCommand
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
 
 
 class TestConsoleDocs(unittest.TestCase):
